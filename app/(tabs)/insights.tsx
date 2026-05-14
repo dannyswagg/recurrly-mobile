b816f8070ede9f8ba10377519f8ea627ed57ebbb
dannyswagg <ugiomohd@yahoo.com>
2026-05-14 22:35:38 +0100

    navigation par and screen pages setup

--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,11 +1,20 @@
+import { styled } from "nativewind";
 import React from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const ContentView = styled(View);
 
 const insights = () => {
   return (
-    <View>
-      <Text>insights</Text>
-    </View>
+    <SafeAreaView
+      edges={["top", "bottom"]}
+      style={{ flex: 1, backgroundColor: "#fff9e3" }}
+    >
+      <ContentView className="flex-1 p-5">
+        <Text>insights</Text>
+      </ContentView>
+    </SafeAreaView>
   );
 };
 
