2a2505fe4f10470f7a2b9dfd191936c7933c089a
dannyswagg <ugiomohd@yahoo.com>
2026-05-15 11:11:59 +0100

    setup font typography

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,19 @@
+import { styled } from "nativewind";
 import React from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+const ContentView = styled(View);
 
 const signUp = () => {
   return (
-    <View>
-      <Text>signUp</Text>
-    </View>
+    <SafeAreaView
+      edges={["top", "bottom"]}
+      style={{ flex: 1, backgroundColor: "#fff9e3" }}
+    >
+      <ContentView className="flex-1 p-5">
+        <Text>signUp</Text>
+      </ContentView>
+    </SafeAreaView>
   );
 };
 
